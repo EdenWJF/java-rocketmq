061145cfe96dcdb74179b5af7924988502f301e2
wangjianfa <wangjianfa@oneship.cc>
2019-03-19 20:53:26 +0800

    first commit

---
+++ src/main/java/com/jinfour/rocketmq/RocketmqApplication.java
@@ -0,0 +1,14 @@
+package com.jinfour.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/jinfour/rocketmq/example/quickstart/Producer.java
@@ -0,0 +1,9 @@
+package com.jinfour.rocketmq.example.quickstart;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+
+public class Producer {
+    public static void main(String[] args){
+        DefaultMQProducer producer = new DefaultMQProducer();
+    }
+}
